eaad0c528101a299aa43debd9fb756714874c915
angrymidget4728 <139581466+angrymidget4728@users.noreply.github.com>
2024-05-26 21:06:46 +0600

    Add partials and bootstrap navbar plug logo

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { title: "Home" });
 })
 
 app.listen(port, () => {
