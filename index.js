0f87e0480d158f7aaf024893daddc9afee235406
angrymidget4728 <139581466+angrymidget4728@users.noreply.github.com>
2024-05-27 16:22:58 +0600

    Create navbar styling and routing, but not all dropdown links

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const navlinksMain = {
+    "/": "Home",
+    "/who-we-are": "Who We Are",
+    "/contact-us": "Contact Us",
+    whatWeDo: {
+        "/our-services": "Our Services",
+        "/business-functions": "Business Functions",
+    }
+}
 
 app.use(express.static("public"));
 
 // Homepage
 app.get("/", (req, res) => {
-    res.render("index.ejs", { title: "Home" });
+    res.render("index.ejs", { navlinksMain: navlinksMain, currentUrl: req.url });
+})
+
+// Who we are
+app.get("/who-we-are", (req, res) => {
+    res.render("who-we-are.ejs", { navlinksMain: navlinksMain, currentUrl: req.url });
+})
+
+// Contact Us
+app.get("/contact-us", (req, res) => {
+    res.render("contact-us.ejs", { navlinksMain: navlinksMain, currentUrl: req.url });
 })
 
 // What we do
 app.get("/our-services", (req, res) => {
-    res.render("our-services.ejs", { title: "Our Services" });
+    res.render("our-services.ejs", { navlinksMain: navlinksMain.whatWeDo, currentUrl: req.url });
 })
 
 app.get("/business-functions", (req, res) => {
-    res.render("business-functions.ejs", { title: "Business Functions" });
+    res.render("business-functions.ejs", { navlinksMain: navlinksMain.whatWeDo, currentUrl: req.url });
 })
 
 app.get("/industry-expertise", (req, res) => {
@@ -29,11 +48,6 @@ app.get("/technology", (req, res) => {
 
 app.get("/saas", (req, res) => {
     res.render("saas.ejs", { title: "SaaS" });
-})
-
-// Contact Us
-app.get("/contact-us", (req, res) => {
-    res.render("contact-us.ejs", { title: "Contact Us" });
 })
 
 app.listen(port, () => {
