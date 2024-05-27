f125c8f921d693355e53543210c76b00f180ada5
angrymidget4728 <139581466+angrymidget4728@users.noreply.github.com>
2024-05-27 10:52:12 +0600

    Add pages except blogs and working in the company

--- index.js
+++ index.js
@@ -5,8 +5,35 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+// Homepage
 app.get("/", (req, res) => {
     res.render("index.ejs", { title: "Home" });
+})
+
+// What we do
+app.get("/our-services", (req, res) => {
+    res.render("our-services.ejs", { title: "Our Services" });
+})
+
+app.get("/business-functions", (req, res) => {
+    res.render("business-functions.ejs", { title: "Business Functions" });
+})
+
+app.get("/industry-expertise", (req, res) => {
+    res.render("industry-expertise.ejs", { title: "Industry Expertise" });
+})
+
+app.get("/technology", (req, res) => {
+    res.render("technology.ejs", { title: "Technology" });
+})
+
+app.get("/saas", (req, res) => {
+    res.render("saas.ejs", { title: "SaaS" });
+})
+
+// Contact Us
+app.get("/contact-us", (req, res) => {
+    res.render("contact-us.ejs", { title: "Contact Us" });
 })
 
 app.listen(port, () => {
